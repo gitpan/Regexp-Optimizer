3173d6aba881d85b0d147327672dc031e3451635
Dan Kogai <dankogai+cpan@gmail.com>
2013-02-23 13:47:59 +0000

    Import of DANKOGAI/Regexp-Optimizer-0.20 from CPAN.
    gitpan-cpan-distribution: Regexp-Optimizer
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         DANKOGAI/Regexp-Optimizer-0.20.tar.gz
    gitpan-cpan-author:       DANKOGAI
    gitpan-cpan-maturity:     released

--- lib/Regexp/Optimizer.pm
+++ lib/Regexp/Optimizer.pm
@@ -1,422 +1,204 @@
-#
-# $Id: Optimizer.pm,v 0.16 2013/02/20 08:53:16 dankogai Exp dankogai $
-#
 package Regexp::Optimizer;
-use 5.006; # qr/(??{}/ needed
+
+use 5.008001;
 use strict;
-use warnings;
-use base qw/Regexp::List/;
-our $VERSION = do { my @r = (q$Revision: 0.16 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r };
+use warnings FATAL => 'all';
+use Regexp::Assemble;
+our $VERSION = sprintf "%d.%02d", q$Revision: 0.20 $ =~ /(\d+)/g;
 
-#our @EXPORT = qw();
-#our %EXPORT_TAGS = ( 'all' => [ qw() ] );
-#our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-#our $DEBUG     = 0;
+my $re_nested;
+$re_nested = qr{
+  (?<=[^\\])\(           # unescaped open paren
+  (?:
+    (?>[^()]+)           # Non-parens w/o backtracking
+    |                    # or
+    (??{ $re_nested })   # Group with matching parens
+   )*
+  \)                     # close paren
+}msx;
 
-# see perldoc perlop
+my $re_optimize = qr{(?<=[^\\])\|}msx;
 
-# perldoc perlop on perl 5.8.4 or later
-#
-#  Pragmata are now correctly propagated into (?{...}) constructions in
-#  regexps.  Code such as
-#
-#    my $x = qr{ ... (??{ $x }) ... };
-#
-#   will now (correctly) fail under use strict. (As the inner $x is 
-#   and has always referred to $::x)
-
-our $RE_PAREN; # predeclear
-$RE_PAREN = 
-    qr{
-       \(
-       (?:
-	(?> [^()]+ )
-	|
-	(??{ $RE_PAREN })
-       )*
-       \)
-      }xo;
-our $RE_EXPR; # predeclear
-$RE_EXPR = 
-    qr{
-       \{
-       (?:
-	(?> [^{}]+ )
-	|
-	(??{ $RE_EXPR })
-       )*
-       \}
-      }xo;
-our $RE_PIPE = qr/(?!\\)\|/o;
-our $RE_CHAR = 
-    qr{(?:
-	# single character...
-	(?!\\)[^\\\[(|)\]]       | # raw character except '[(|)]'
-	$Regexp::List::RE_XCHAR  | # extended characters
-       )}xo;
-our $RE_CCLASS = 
-    qr{(?:
-	(?!\\)\[ $RE_CHAR+? \] |
-	$Regexp::List::RE_XCHAR      | # extended characters
-	(?!\\)[^(|)]                 | # raw character except '[(|)]'
-	# Note pseudo-characters are not included
-    )}xo;
-our $RE_QUANT =
-    qr{(?:
-	(?!\\)
-	    (?:
-	     \? |
-	     \+ |
-	     \* |
-	     \{[\d,]+\}
-	     )\??
-	)}xo;
-our $RE_TOKEN = 
-    qr{(?:
-	(?:
-	\\[ULQ] (?:$RE_CHAR+)(?:\\E|$) | # [ul]c or quotemeta
-        $Regexp::List::RE_PCHAR  | # pseudo-characters
-        $RE_CCLASS |
-	$RE_CHAR     
-       )
-	 $RE_QUANT?
-       )}xo;
-our $RE_START = $Regexp::List::RE_START;
-
-our %PARAM = (meta      => 1,
-	      quotemeta => 0,
-	      lookahead => 0,
-	      optim_cc  => 1,
-	      modifiers => '',
-	      _char     => $RE_CHAR,
-	      _token    => $RE_TOKEN,
-	      _cclass   => $RE_CCLASS,
-	     );
-
-sub new{
-    my $class = ref $_[0] ? ref shift : shift;
-    my $self = $class->SUPER::new;
-    $self->set(%PARAM, @_);
-    $self;
+sub new {
+    my $class = shift;
+    bless {@_}, $class;
 }
 
-sub list2re{
-    shift->SUPER::list2re(map {_strip($_)} @_);
-}
-
-sub optimize{
-    my $self = shift;
-    my $str  = shift;
-    $self->{unexpand} and $str = $self->unexpand($str);
-    # safetey feature against qq/(?:foo)(?:bar)/
-    !ref $str and $str =~ /^$RE_START/ and $str = qr/$str/;
-    my $re = $self->_optimize($str);
-    qr/$re/;
-}
-
-sub _strip{
-    my ($str, $force) = @_;
-    $force or ref $str eq 'Regexp' or return $str;
-    $str =~ s/^($RE_START)//o or return $str;
-    my $regopt = $1;  $str =~ s/\)$//o;
-    $regopt =~ s/^\(\??//o; 
-    $regopt =~ /^[-:]/ and $regopt = undef;
-    ($str, $regopt);
-}
-
-my %my_l2r_opts = 
-    (
-     as_string => 1, 
-     debug     => 0,
-     _token    => qr/$RE_PAREN$RE_QUANT?|$RE_PIPE|$RE_TOKEN/,
-    );
-
-sub _optimize{
-    no warnings 'uninitialized';
-    my $self = shift;
-    $self->{debug} and $self->{_indent}++;
-    $self->{debug} and
-	print STDERR '>'x $self->{_indent}, " ", $_[0], "\n";
-    my ($result, $regopt)  = _strip(shift, 1);
-    $result =~ s/\\([()])/"\\x" . sprintf("%X", ord($1))/ego;
-    # $result =~ s/(\s)/"\\x" . sprintf("%X", ord($1))/ego;
-    $result !~ /$RE_PIPE/ and goto RESULT;
-    my $l = $self->clone->set(%my_l2r_opts);
-    # optimize
-    unless ($result =~ /$RE_PAREN/){
-        my @words = split /$RE_PIPE/ => $result;
-        $result = $l->list2re(@words);
-	goto RESULT;
+sub _assemble {
+    my $str = shift;
+    return $str if $str !~ $re_optimize;
+    if ( $str =~ s{\A(\((?:\?.*?:)?)}{}msx ) {
+        my $op = $1;
+        $str =~ s/\)\z//msx;
+        return $op . _assemble($str) . ')';
     }
-    my (@term, $sp);
-    while ($result){
-	if ($result =~ s/^($RE_PAREN)($RE_QUANT?)//){
-	    my ($term, $quant) = ($1, $2);
-	    $term = $self->_optimize($term);
-	    $l->{optim_cc} = $quant ? 0 : 1;
-	    if ($quant){
-		if ($term =~ /^$self->{_cclass}$/){
-		    $term .= $quant;
-		}else{
-		    $term = $self->{po} . $term . $self->{pc} . $quant;
-		}
-	    }
-	    $term[$sp] .= $term;
-	}elsif($result =~ s/^$RE_PIPE//){
-	    $sp += 2;
-	    push @term, '|';
-	}elsif($result =~ s/^($RE_TOKEN+)//){
-	    # warn $1;
-	    $term[$sp] .= $1;
-	}else{
-	    die "something is wrong !";
-	}
+    elsif ( $str =~ m{(?<=[^\\])\(}msx ) {
+        $str =~ s/($re_nested)/_assemble($1)/msxge;
+        return $str;
     }
-    # warn scalar @term , ";", join(";" => @term);
-    # sleep 1;
-    my @stack;
-    while (my $term = shift @term){
-	if ($term eq '|'){
-	    push @stack, $l->list2re(pop @stack, shift @term);
-	}else{
-	    push @stack, $term;
-	}
+    else {
+        my $ra = Regexp::Assemble->new();
+        $ra->add( split m{(?<=[^\\])\|}, $str );
+        return $ra->as_string;
     }
-    $result = join('' => @stack);
- RESULT:
-    $result =  $regopt ? qq/(?$regopt$result)/ : $result;
-    # warn qq($result, $regopt);
-    $self->{debug} and 
-	print STDERR '<'x $self->{_indent}, " ", $result, "\n";
-    $self->{debug} and $self->{_indent}--;
-    $result;
 }
 
-sub _pair2re{
+sub as_string {
+    my ( $self, $str ) = @_;
+    return $str if $str !~ $re_optimize;
+    $str =~ s/\A\(\?(.*?)://;
+    my $mod = $1;
+    $str =~ s/\)\z//;
+    if ( $mod =~ /x/ ) {
+        $str =~ s{^\s+}{}mg;
+        $str =~ s{(?<=[^\\])\s*?#.*?$}{}mg;
+        $str =~ s{(?:\r\n?|\n)}{}msg;
+    }
+    $str = _assemble($str);
+    "(?$mod:$str)";
+}
+
+sub optimize {
     my $self = shift;
-    $_[0] eq $_[1] and return $_[0];
-    my ($first, $second) =
-	length $_[0] <= length $_[1] ? @_ : ($_[1], $_[0]);
-    my $l = length($first);
-    $l -= 1
-	while $self->_head($first, $l) ne $self->_head($second, $l);
-    $l > 0 or return join("", @_);
-    return $self->_head($first, $l) . 
-	$self->{po} . 
-	$self->_tail($first, $l) . '|' . $self->_tail($second, $l) .
-	$self->{pc};
+    my $re   = $self->as_string(@_);
+    qr{$re};
 }
 
-1;
+1; # End of Regexp::Optimizer
+
 __END__
-
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
 Regexp::Optimizer - optimizes regular expressions
 
-=head1 OBSOLETED BY
+=head1 VERSION
 
-L<Regexp::Assemble>
+$Id: Optimizer.pm,v 0.20 2013/02/23 13:43:59 dankogai Exp dankogai $
 
 =head1 SYNOPSIS
 
   use Regexp::Optimizer;
-  my $o  = Regexp::Optimizer->new;
-  my $re = $o->optimize(qr/foobar|fooxar|foozap/);
+  my $o  = Regexp::Optimizer->new->optimize(qr/foobar|fooxar|foozap/);
   # $re is now qr/foo(?:[bx]ar|zap)/
 
-=head1 ABSTRACT
+=head1 EXPORT
 
-This module does, ahem, attempts to, optimize regular expressions.
+none.
 
-=head1 INSTALLATION
+=head1 SUBROUTINES/METHODS
 
-To install this module type the following:
+=head2 new
 
-   perl Makefile.PL
-   make
-   make test
-   make install
+Makes a new optimizer instance.
 
-=head1 DESCRIPTION
+  my $ro = Regexp::Optimizer->new;
 
-Here is a quote from L<perltodo>.
+=head2 optimize
 
-  Factoring out common suffices/prefices in regexps (trie optimization)
+Does the optimization.
 
-  Currently, the user has to optimize "foo|far" and "foo|goo" into
-  "f(?:oo|ar)" and "[fg]oo" by hand; this could be done automatically.
+  my $re = qr/foobar|fooxar|foozap/;
+  $re = $ro->optimize($re);
 
-This module implements just that.
+If it is already optimized -- no alteration in the regexp, it is
+practically an identity function which simply returns an argument.
 
-=head2 EXPORT
+If not, it dissasembles the regexp, feeds it to L<Regexp::Assemble>,
+and reassembles the result.
 
-Since this is an OO module there is no symbol exported.
+=head2 as_string
 
-=head1 METHODS
+Same as C<optimize()> but returns a string instead of regexp object.
 
-This module is implemented as a subclass of L<Regexp::List>.  For
-methods not listed here, see L<Regexp::List>.
+=head1 CAVEAT
 
-=over
-
-=item $o  = Regexp::Optimizer->new;
-
-=item $o->set(I<< key => value, ... >>)
-
-Just the same us L<Regexp::List> except for the attribute below;
-
-=over
-
-=item unexpand
-
-When set to one, $o->optimize() tries to $o->expand before actually
-starting the operation.
-
-  # cases you need to set expand => 1
-  $o->set(expand => 1)->optimize(qr/
-                                   foobar|
-                                   fooxar|
-                                   foozar
-                                   /x);
-
-=back
-
-=item $re = $o->optimize(I<regexp>);
-
-Does the job.  Note that unlike C<< ->list2re() >> in L<Regexp::List>,
-the argument is the regular expression itself.  What it basically does
-is to find groups will alterations and replace it with the result of
-C<< $o->list2re >>.
-
-=item $re = $o->list2re(I<list of words ...>)
-
-Same as C<list2re()> in L<Regexp::List> in terms of functionality but
-how it tokenize "atoms" is different since the arguments can be
-regular expressions, not just strings.  Here is a brief example.
-
-  my @expr = qw/foobar fooba+/;
-  Regexp::List->new->list2re(@expr) eq qr/fooba[\+r]/;
-  Regexp::Optimizer->new->list2re(@expr) eq qr/foob(?:a+ar)/;
-
-=back
-
-=head1 CAVEATS
-
-This module is still experimental.  Do not assume that the result is
-the same as the unoptimized version.
-
-=over
-
-=item *
-
-When you just want a regular expression which matches normal words
-with not metacharacters, use <Regexp::List>.  It's more robus and 
-much faster.
-
-=item *
-
-When you have a list of regular expessions which you want to
-aggregate, use C<list2re> of THIS MODULE.
-
-=item *
-
-Use C<< ->optimize() >> when and only when you already have a big
-regular expression with alterations therein.
-
-C<< ->optimize() >> does support nested groups but its parser is not
-tested very well.
-
-=back
-
-=head1 BUGS
-
-=over
-
-=item *
-
-Regex parser in this module (which itself is implemented by regular
-expression) is not as thoroughly tested as L<Regexp::List>
-
-=item *
-
-May still fall into deep recursion when you attempt to optimize
-deeply nested regexp.  See L</PRACTICALITY>.
-
-=item *
-
-Does not grok (?{expression}) and (?(cond)yes|no) constructs yet
-
-=item *
-
-You need to escape characters in character classes.
-
-  $o->optimize(qr/[a-z()]|[A-Z]/);              # wrong
-  $o->optimize(qr/[a-z\(\)]|[A-Z]/);            # right
-  $o->optimize(qr/[0-9A-Za-z]|[\Q-_.!~*"'()\E]/ # right, too. 
-
-=item *
-
-When character(?: class(?:es)?)? are aggregated, duplicate ranges are
-left as is.  Though functionally OK, it is cosmetically ugly.
-
-  $o->optimize(qr/[0-5]|[5-9]|0123456789/);
-  # simply turns into [0-5][5-9]0123456789] not [0-9]
-
-I left it that way because marking-rearranging approach can result a
-humongous result when unicode characters are concerned (and
-\p{Properties}).
-
-=back
-
-=head1 PRACTICALITY
-
-Though this module is still experimental, It is still good enough even
-for such deeply nested regexes as the followng.
-
-  # See 3.2.2 of  http://www.ietf.org/rfc/rfc2616.txt
-  # BNF faithfully turned into a regex
-  http://(?:(?:(?:(?:(?:[a-z]|[A-Z])|[0-9])|(?:(?:[a-z]|[A-Z])|[0-9])(?:(?:(?:[a-z]|[A-Z])|[0-9])|-)*(?:(?:[a-z]|[A-Z])|[0-9]))\.)*(?:(?:[a-z]|[A-Z])|(?:[a-z]|[A-Z])(?:(?:(?:[a-z]|[A-Z])|[0-9])|-)*(?:(?:[a-z]|[A-Z])|[0-9]))\.?|[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(?::[0-9]*)?(?:/(?:(?:(?:(?:[a-z]|[A-Z])|[0-9])|[\-\_\.\!\~\*\'\(\)])|%(?:[0-9]|[A-Fa-f])(?:[0-9]|[A-Fa-f])|[:@&=+$,])*(?:;(?:(?:(?:(?:[a-z]|[A-Z])|[0-9])|[\-\_\.\!\~\*\'\(\)])|%(?:[0-9]|[A-Fa-f])(?:[0-9]|[A-Fa-f])|[:@&=+$,])*)*(?:/(?:(?:(?:(?:[a-z]|[A-Z])|[0-9])|[\-\_\.\!\~\*\'\(\)])|%(?:[0-9]|[A-Fa-f])(?:[0-9]|[A-Fa-f])|[:@&=+$,])*(?:;(?:(?:(?:(?:[a-z]|[A-Z])|[0-9])|[\-\_\.\!\~\*\'\(\)])|%(?:[0-9]|[A-Fa-f])(?:[0-9]|[A-Fa-f])|[:@&=+$,])*)*)*(?:\\?(?:[;/?:@&=+$,]|(?:(?:(?:[a-z]|[A-Z])|[0-9])|[\-\_\.\!\~\*\'\(\)])|%(?:[0-9]|[A-Fa-f])(?:[0-9]|[A-Fa-f]))*)?)?
-
-  # and optimized
-  http://(?::?[a-zA-Z0-9](?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?\.[a-zA-Z]*(?:[a-zA-Z0-9\-]*[a-zA-Z0-9])?\.?|[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(?::[0-9]*)?(?:/(?:(?:(?:[a-zA-Z0-9\-\_\.\!\~\*\'\x28\x29]|%[0-9A-Fa-f][0-9A-Fa-f])|[:@&=+$,]))*(?:;(?:(?:(?:[a-zA-Z0-9\-\_\.\!\~\*\'\x28\x29]|%[0-9A-Fa-f][0-9A-Fa-f])|[:@&=+$,]))*)*(?:/(?:(?:(?:[a-zA-Z0-9\-\_\.\!\~\*\'\x28\x29]|%[0-9A-Fa-f][0-9A-Fa-f])|[:@&=+$,]))*(?:;(?:(?:(?:[a-zA-Z0-9\-\_\.\!\~\*\'\x28\x29]|%[0-9A-Fa-f][0-9A-Fa-f])|[:@&=+$,]))*)*)*(?:\\?(?:(?:[;/?:@&=+$,a-zA-Z0-9\-\_\.\!\~\*\'\x28\x29]|%[0-9A-Fa-f][0-9A-Fa-f]))*)?)?
-
-By carefully examine both you can find that character classes are
-properly aggregated.
+This module depends on the C<??{CODE}> regexp construct which is still
+considered experimental as of Perl 5.16.
 
 =head1 SEE ALSO
 
-L<Regexp::List> -- upon which this module is based
-
-C<eg/> directory in this package contains example scripts.
-
-=over
-
-=item Perl standard documents
-
- L<perltodo>, L<perlre>
-
-=item CPAN Modules
-
-L<Regexp::Presuf>, L<Text::Trie>
-
-=item Books
-
-Mastering Regular Expressions  L<http://www.oreilly.com/catalog/regex2/>
-
-=back
+L<Regexp::Assemble>, L<perlre>
 
 =head1 AUTHOR
 
-Dan Kogai <dankogai@dan.co.jp>
+Dan Kogai, C<< <dankogai+cpan at gmail.com> >>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 BUGS
 
-Copyright 2003 by Dan Kogai
+Please report any bugs or feature requests to C<bug-regexp-optimizer at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Regexp-Optimizer>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. 
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Regexp::Optimizer
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Regexp-Optimizer>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Regexp-Optimizer>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Regexp-Optimizer>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Regexp-Optimizer/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Dan Kogai.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 =cut
--- t/01-use.t
+++
@@ -1,9 +0,0 @@
-#
-# $Id: 01-use.t,v 0.1 2003/05/31 10:43:20 dankogai Exp $
-#
-use strict;
-use warnings;
-use Test::More tests => 2;
-use_ok('Regexp::Optimizer');
-my $o = new Regexp::Optimizer;
-isa_ok($o,'Regexp::List');
